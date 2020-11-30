6ea2fa056803f63ccd7705f512e767dd3253960f
Bruno Steinmann <bruno.steinmann@gmx.com>
2020-11-30 08:00:20 +0100

    configured signin component and axios

--- src/components/Signin.vue
+++ src/components/Signin.vue
@@ -9,10 +9,15 @@
       </v-card-text>
       <v-divider></v-divider>
       <v-form>
-        <v-text-field label="Username" prepend-icon="mdi-account-circle" />
+        <v-text-field
+          label="Email"
+          v-model="email"
+          prepend-icon="mdi-account-circle"
+        />
         <v-text-field
           :type="showPassword ? 'text' : 'password'"
           label="Password"
+          v-model="password"
           prepend-icon="mdi-lock"
           :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
           @click:append="showPassword = !showPassword"
@@ -20,7 +25,7 @@
       </v-form>
       <v-divider></v-divider>
       <v-card-actions>
-        <v-btn color="info">Login</v-btn>
+        <v-btn color="info" @click="signin">Login</v-btn>
         <v-spacer></v-spacer>
         <v-btn color="info">Register</v-btn>
       </v-card-actions>
@@ -30,11 +35,50 @@
 
 <script>
 export default {
-  name: "App",
+  name: "Signin",
   data() {
     return {
+      email: "",
+      password: "",
+      error: "",
       showPassword: false
     };
+  },
+  created() {
+    this.checkSignedIn();
+  },
+  updated() {
+    this.checkSignedIn();
+  },
+  methods: {
+    signin() {
+      this.$http.plain
+        .post("/signin", { email: this.email, password: this.password })
+        .then(response => this.signinSuccessful(response))
+        .catch(error => this.signinFailed(error));
+    },
+    signinSuccessful(response) {
+      if (!response.data.csrf) {
+        this.signinFailed(response);
+        return;
+      }
+      localStorage.csrf = response.data.csrf;
+      localStorage.signedIn = true;
+      this.error = "";
+      this.$router.replace("/todos");
+    },
+    signinFailed(error) {
+      this.error =
+        (error.response && error.response.data && error.response.data.error) ||
+        "";
+      delete localStorage.csrf;
+      delete localStorage.signedIn;
+    },
+    checkSignedIn() {
+      if (localStorage.signedIn) {
+        this.$router.replace("/todos");
+      }
+    }
   }
 };
 </script>
--- src/main.js
+++ src/main.js
@@ -2,15 +2,21 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import VueAxios from "vue-axios";
-import axios from "@/backend/axios";
+// import axios from "@/backend/axios";
 import store from "./store";
 import vuetify from "./plugins/vuetify";
+import { securedAxiosInstance, plainAxiosInstance } from "@/backend/axios";
 
 Vue.config.productionTip = false;
-Vue.use(VueAxios, axios);
+Vue.use(VueAxios, {
+  secured: securedAxiosInstance,
+  plain: plainAxiosInstance
+});
 
 new Vue({
   router,
+  securedAxiosInstance,
+  plainAxiosInstance,
   store,
   vuetify,
   render: h => h(App)
